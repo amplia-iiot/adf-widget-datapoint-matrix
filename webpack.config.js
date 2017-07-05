4f4ae19166f2f54db15879ee9952d0423c19eb18
Victor Gutierrez <victor.gutierrez@amplia.es>
2017-07-05 13:53:31 +0200

    Add webpack, renamed repo

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require("path");
+
+var config = {
+    entry: {
+        app: [
+            './src/datapointMatrix.js',
+            './src/edit.html',
+            './src/view.html'
+        ],
+        vendor: ['angular-google-chart']
+    },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+                test: /\.css$/,
+                loader: 'style!css',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.bundle.js'
+        })
+    ]
+};
+
+module.exports = config;
