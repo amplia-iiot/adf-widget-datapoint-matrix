bf6381e23dad1fda366fff204f8c352544e0e0a3
Victor Gutierrez <victor.gutierrez@amplia.es>
2017-07-18 14:56:03 +0200

    Cipher password stdin input

--- scripts/utils/read-config-stdin.js
+++ scripts/utils/read-config-stdin.js
@@ -14,18 +14,55 @@ module.exports.readConfiguration = function(cb) {
 
 function readNextKey(keys, labels, config, cb) {
     var key = keys.pop();
-    console.log(labels[key]);
-    stdin.addListener("data", function(d) {
-        // note:  d is an object, and when converted to a string it will
-        // end with a linefeed.  so we (rather crudely) account for that  
-        // with toString() and then trim() 
+    if (key === "password") {
+        hidden(labels[key], function(password) {
+            valueTyped(password);
+        });
+    } else {
+        console.log(labels[key]);
+        stdin.addListener("data", function(d) {
+            // note:  d is an object, and when converted to a string it will
+            // end with a linefeed.  so we (rather crudely) account for that  
+            // with toString() and then trim() 
+            valueTyped(d.toString().trim());
+        });
+    }
+
+    function valueTyped(value) {
         stdin.removeAllListeners('data');
-        config[key] = d.toString().trim();
+        config[key] = value;
         if (keys.length > 0) {
             readNextKey(keys, labels, config, cb);
         } else {
             process.stdin.unref();
             cb(config);
         }
+    }
+}
+
+
+
+function hidden(query, callback) {
+    var readline = require('readline');
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    process.stdin.on("data", function(char) {
+        char = char + "";
+        switch (char) {
+            case "\n":
+            case "\r":
+            case "\u0004":
+                stdin.pause();
+                break;
+            default:
+                process.stdout.write("\033[2K\033[200D" + query + Array(rl.line.length + 1).join("*"));
+                break;
+        }
+    });
+    rl.question(query, function(value) {
+        rl.history = rl.history.slice(1);
+        callback(value);
     });
 }
