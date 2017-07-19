978d99fbdfa42a5326eff206c97cbdef1e733139
Victor Gutierrez <victor.gutierrez@amplia.es>
2017-07-19 08:50:30 +0200

    Remove prefix on angular-templatecache-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ var config = {
             },
             {
                 test: /\.html$/,
-                loader: "angular-templatecache-loader?module=adf.widget.datapointMatrix&prefix=/view"
+                loader: "angular-templatecache-loader?module=adf.widget.datapointMatrix"
             }
         ]
     },
